351ac27285a24b1d929cfb1c7e762806bd46e676
Evgeniy Kuzmin <jekccs@gmail.com>
2016-07-10 14:24:45 +0300

    Main logger spec

--- spec/Gamma/ApiLogger/Service/LoggerStopwatchSpec.php
+++ spec/Gamma/ApiLogger/Service/LoggerStopwatchSpec.php
@@ -6,12 +6,14 @@ use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
-
+use Symfony\Component\Stopwatch\StopwatchEvent;
 
 class LoggerStopwatchSpec extends ObjectBehavior
 {
     const LOGGER_ENABLED = true;
     const LOGGER_DISABLED = false;
+    const TEST_API_METHOD = "testName";
+    const EXPECTED_DURATION = 100;
 
     function it_is_initializable()
     {
@@ -23,8 +25,38 @@ class LoggerStopwatchSpec extends ObjectBehavior
         $this->shouldImplement('Gamma\ApiLogger\Service\LoggerStopwatchInterface');
     }
 
-    function let(Stopwatch $stopwatch, LoggerInterface $logger)
+    function let(Stopwatch $stopwatch, LoggerInterface $logger, StopwatchEvent $event)
     {
+        $event->getDuration()->willReturn(self::EXPECTED_DURATION);
+        $stopwatch->start(self::TEST_API_METHOD)->willReturn($event);
+        $stopwatch->stop(self::TEST_API_METHOD)->willReturn($event);
         $this->beConstructedWith($stopwatch, $logger, self::LOGGER_ENABLED);
+    }
+
+    function it_starts_logging_if_enabled(Stopwatch $stopwatch)
+    {
+        $stopwatch->start(self::TEST_API_METHOD)->shouldBeCalled();
+        $this->start(self::TEST_API_METHOD);
+    }
+
+    function it_does_not_start_logging_if_disabled(Stopwatch $stopwatch, LoggerInterface $logger)
+    {
+        $this->beConstructedWith($stopwatch, $logger, self::LOGGER_DISABLED);
+        $stopwatch->start(self::TEST_API_METHOD)->shouldNotBeCalled();
+        $this->start(self::TEST_API_METHOD);
+    }
+
+    function it_stops_logging_if_enabled(Stopwatch $stopwatch, StopwatchEvent $event, LoggerInterface $logger)
+    {
+        $stopwatch->stop(self::TEST_API_METHOD)->shouldBeCalled();
+        $logger->info(self::TEST_API_METHOD.",                ".self::EXPECTED_DURATION."ms", Argument::any())->shouldBeCalled();
+        $this->stop(self::TEST_API_METHOD)->shouldReturn($event);
+    }
+
+    function it_breaks_stop_action_if_disabled(Stopwatch $stopwatch, LoggerInterface $logger)
+    {
+        $this->beConstructedWith($stopwatch, $logger, self::LOGGER_DISABLED);
+        $stopwatch->stop(self::TEST_API_METHOD)->shouldNotBeCalled();
+        $this->stop(self::TEST_API_METHOD);
     }
 }
